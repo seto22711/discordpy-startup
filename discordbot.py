4380b12efd8005711e159b60241e331daf53d365
seto22711 <orphanwataru@yahoo.co.jp>
2020-06-12 11:21:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -28,7 +28,9 @@ async def on_message(message):
         # 入力された内容を受け取る
         say = message.content 
 
-        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
+        await ctx.send('input' + say)
+        
+        # [/dice ]部分を消し、AdBのdで区切ってリスト化する
         order = say.strip('/dice ')
         cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
         dice = diceroll(cnt, mx) # 和を計算する関数(後述)
