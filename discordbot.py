d1077e49530d179a6e8e3baea6f2c7995c85b8d1
seto22711 <orphanwataru@yahoo.co.jp>
2020-06-12 11:12:44 +0900

    diceroll

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,9 @@ from discord.ext import commands
 import os
 import traceback
 
+# さいころの和を計算する用の関数
+from func import  diceroll
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -16,6 +19,24 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith("!dice"):
+        # 入力された内容を受け取る
+        say = message.content 
+
+        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
+        order = say.strip('!dice ')
+        cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
+        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
+        await message.channel.send(dice[cnt])
+        del dice[cnt]
+
+        # さいころの目の総和の内訳を表示する
+        await message.channel.send(dice)
 
 
 bot.run(token)
